9df4b91569dfbb702e10a4a2b62ce19292daab2e
offsujin <offsujin@gmail.com>
2022-04-21 19:33:26 +0900

    Initial commit

---
+++ src/test/java/inflearn/spring/core/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
