77b994b64b8cac79649e27e387e381a434a3dfc5
offsujin <offsujin@gmail.com>
2022-05-29 23:46:03 +0900

    Add new DiscountPolicy & Test

---
+++ src/main/java/inflearn/spring/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package inflearn.spring.core.discount;
+
+import inflearn.spring.core.member.Grade;
+import inflearn.spring.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
